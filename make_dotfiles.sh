8f77337ada5c50ecad5252422817fc2feaf363a0
Hugo Loio <loio.2601@gmail.com>
2020-02-02 23:29:09 +0000

    Added bash_profile and profile

--- make_dotfiles.sh
+++ make_dotfiles.sh
@@ -13,4 +13,7 @@ echo ". ~/.config/xinit/xinitrc" > ~/.xinitrc
 echo ". ~/.config/xserver/xserverrc" > ~/.xserverrc 
 
 #make .profile
-cp ~/.config/profile/profile .profile
+cp ~/.config/profile .profile
+
+#make .bash_profile
+cp ~/.config/bash_profile .profile
