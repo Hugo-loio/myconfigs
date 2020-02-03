93f5b5f3acb2cb6e03508a6446d4fdfef6ac226b
Hugo Loio <loio.2601@gmail.com>
2020-02-03 15:53:50 +0000

    lightdm configs

--- make_dotfiles.sh
+++ make_dotfiles.sh
@@ -1,20 +1,36 @@
 #!/bin/sh
 
 #make .vimrc
-echo ":source ~/.config/vim/vimrc" > ~/.vimrc 
+echo ":source $XDG_CONFIG_HOME/vim/vimrc" > ~/.vimrc 
 
 #make .bashrc
-echo ". ~/.config/bash/bashrc" > ~/.bashrc 
+echo ". $XDG_CONFIG_HOME/bash/bashrc" > ~/.bashrc 
 
 #make .xinitrc
-echo ". ~/.config/xinit/xinitrc" > ~/.xinitrc 
+echo ". $XDG_CONFIG_HOME/xinit/xinitrc" > ~/.xinitrc 
+
+#make .xprofile (same as xinitrc)
+echo ". $XDG_CONFIG_HOME/xinit/xinitrc" > ~/.xprofile 
 
 #make .xserverrc
-echo ". ~/.config/xserver/xserverrc" > ~/.xserverrc 
+echo ". $XDG_CONFIG_HOME/xserver/xserverrc" > ~/.xserverrc 
 
 #make .profile
-#cp ~/.config/profile .profile
-echo ". ~/.config/profile" > ~/.profile 
+echo ". $XDG_CONFIG_HOME/profile" > ~/.profile 
 
 #make .bash_profile
-echo ". ~/.config/bash_profile" > ~/.bash_profile 
+echo ". $XDG_CONFIG_HOME/bash_profile" > ~/.bash_profile 
+
+echo "Update lightdm configs (need root permision to be done)?(y/n)"
+read response
+
+if [ "$response" = "y" ]
+then
+  #make lightdm.conf
+  sudo cp $XDG_CONFIG_HOME/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
+
+  #make lightdm-webkit2-greeter.conf
+  sudo cp $XDG_CONFIG_HOME/lightdm/lightdm-webkit2-greeter.conf /etc/lightdm/lightdm-webkit2-greeter.conf
+else
+  echo "Lightdm configs not updated."
+fi
