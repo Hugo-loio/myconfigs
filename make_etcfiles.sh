82e63bab3a3f8e8f54983f13200ac6d200879b5e
Hugo-loio <loio.2601@gmail.com>
2020-03-08 15:27:18 +0000

    added sxhkd keybinding and deleted systemd power config

--- make_etcfiles.sh
+++ make_etcfiles.sh
@@ -2,9 +2,6 @@
 
 echo "This script will use root permisions" 
 
-#Systemd
-sudo cp $XDG_CONFIG_HOME/systemd/logind.conf /etc/systemd/logind.conf
-
 #Xorg
 sudo cp $XDG_CONFIG_HOME/xorg/70-synaptics.conf /etc/X11/xorg.conf.d/70-synaptics.conf
 
