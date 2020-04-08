a86d7664b695950170087a512ed460ec8bff6dfe
Hugo-loio <loio.2601@gmail.com>
2020-04-08 18:25:15 +0100

    Updated package lists

--- pkg_list/list_packages.sh
+++ pkg_list/list_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-pacman -Qen > $XDG_CONFIG_HOME/pkg_list/main_repo_packages.txt
-pacman -Qem > $XDG_CONFIG_HOME/pkg_list/aur_packages.txt
+pacman -Qen > main_repo_packages.txt
+pacman -Qem > aur_packages.txt
