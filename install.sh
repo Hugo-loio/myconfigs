a073ef8001a0bd8895ebace7c9652e6c3c2ee766
Hugo-loio <loio.2601@gmail.com>
2020-08-06 12:37:02 +0100

    Started using rofi

--- install.sh
+++ install.sh
@@ -11,7 +11,8 @@ home_dotfiles="profile xinit/xinitrc xinit/xserverrc \
 config_folder="dunst/dunstrc bspwm/bspwmrc \
   fontconfig/fonts.conf picom/picom.conf polybar/config polybar/launch.sh \
   ranger/commands.py ranger/commands_full.py ranger/rc.conf ranger/rifle.conf \
-  ranger/scope.sh sxhkd/sxhkdrc kitty/kitty.conf mpv/input.conf mpv/mpv.conf"
+  ranger/scope.sh sxhkd/sxhkdrc kitty/kitty.conf mpv/input.conf mpv/mpv.conf \
+  rofi/config rofi/theme.rasi"
 
 #List of snippets for .vim/UltiSnips directory
 vim_snippets=$(ls $repo/vim/snippets/*.snippets)
