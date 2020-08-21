cd44e36d7f79928ec8b238db13a2ae0c9ccf01a7
Hugo-loio <loio.2601@gmail.com>
2020-08-21 02:23:07 +0100

    Completed theme selection (TODO: dunst)

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ config_folder="dunst/dunstrc bspwm/bspwmrc \
   fontconfig/fonts.conf picom/picom.conf polybar/config polybar/launch.sh \
   ranger/commands.py ranger/commands_full.py ranger/rc.conf ranger/rifle.conf \
   ranger/scope.sh sxhkd/sxhkdrc kitty/kitty.conf mpv/input.conf mpv/mpv.conf \
-  rofi/config gtk-3.0/settings.ini"
+  rofi/config rofi/theme.rasi gtk-3.0/settings.ini"
 
 #List of files for .config directory with no subdirectory
 config_folder_no_subdir="betterlockscreen/betterlockscreenrc"
@@ -105,14 +105,6 @@ export CACHE="$HOME/.local/share/hugoconf"
 if [ ! -d $CACHE ] ; then
   mkdir -p $CACHE
 fi
-
-echo "\nInstalling themes..."
-
-[ -d $CACHE/rofi_themes ] || mkdir $CACHE/rofi_themes
-
-for theme in $(ls $repo/rofi/themes) ; do
-  create_symlic $CACHE/rofi_themes/$theme $repo/rofi/themes/$theme
-done
 
 echo "\nCreating other usefull folders..."
 folders="Documents/books Pictures/wallpapers Desktop Movies Downloads"
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,6 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Get color theme to use
+bgcolor=$(grep polybar_bgcolor $CACHE/theme.txt | cut -d= -f2)
+fgcolor=$(grep polybar_fgcolor $CACHE/theme.txt | cut -d= -f2)
+
 # Launch Polybar, using default config location ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep -w "connected" | cut -d " " -f 1); do
@@ -16,7 +20,7 @@ if type "xrandr"; then
     else
       t="right"
     fi
-    monitor=$m tray=$t polybar mybar &
+    monitor=$m tray=$t bg=$bgcolor fg=$fgcolor polybar mybar &
   done
 else
   polybar mybar &
