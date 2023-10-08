c21ec1fc3567c1a14bc7380feedfcaf919872d73
Hugo Lóio <loio.2601@gmail.com>
2023-10-08 11:39:00 +0200

    automatic gpu card detection for backlight module

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -10,6 +10,8 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 bgcolor=$(grep polybar_bgcolor $CACHE/theme.txt | cut -d= -f2)
 fgcolor=$(grep polybar_fgcolor $CACHE/theme.txt | cut -d= -f2)
 
+bl_card=$(ls /sys/class/backlight || echo "")
+
 # Launch Polybar, using default config location ~/.config/polybar/config
 if type "xrandr"; then
   for m in $(xrandr --query | grep -w "connected" | cut -d " " -f 1); do
@@ -20,7 +22,7 @@ if type "xrandr"; then
     else
       t="right"
     fi
-    monitor=$m tray=$t bg=$bgcolor fg=$fgcolor polybar mybar &
+    monitor=$m tray=$t bg=$bgcolor fg=$fgcolor card=$bl_card polybar mybar &
   done
 else
   polybar mybar &
